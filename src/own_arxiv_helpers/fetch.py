778d93979c5bd532a38e773f56d461fe4502d46f
lukexlook <luke-look@outlook.com>
2025-10-15 11:47:27 +0800

    format and fix deps

--- src/own_arxiv_helpers/fetch.py
+++ src/own_arxiv_helpers/fetch.py
@@ -1,14 +1,26 @@
-def get_arxiv_paper(query:str, debug:bool=False) -> list[ArxivPaper]:
-    client = arxiv.Client(num_retries=10,delay_seconds=10)
+import arxiv
+import feedparser
+from loguru import logger
+from tqdm import tqdm
+
+from .paper import ArxivPaper
+
+
+def get_arxiv_paper(query: str, debug: bool = False) -> list[ArxivPaper]:
+    client = arxiv.Client(num_retries=10, delay_seconds=10)
     feed = feedparser.parse(f"https://rss.arxiv.org/atom/{query}")
-    if 'Feed error for query' in feed.feed.title:
+    if "Feed error for query" in feed.feed.title:
         raise Exception(f"Invalid ARXIV_QUERY: {query}.")
     if not debug:
         papers = []
-        all_paper_ids = [i.id.removeprefix("oai:arXiv.org:") for i in feed.entries if i.arxiv_announce_type == 'new']
-        bar = tqdm(total=len(all_paper_ids),desc="Retrieving Arxiv papers")
-        for i in range(0,len(all_paper_ids),50):
-            search = arxiv.Search(id_list=all_paper_ids[i:i+50])
+        all_paper_ids = [
+            i.id.removeprefix("oai:arXiv.org:")
+            for i in feed.entries
+            if i.arxiv_announce_type == "new"
+        ]
+        bar = tqdm(total=len(all_paper_ids), desc="Retrieving Arxiv papers")
+        for i in range(0, len(all_paper_ids), 50):
+            search = arxiv.Search(id_list=all_paper_ids[i : i + 50])
             batch = [ArxivPaper(p) for p in client.results(search)]
             bar.update(len(batch))
             papers.extend(batch)
@@ -16,7 +28,9 @@ def get_arxiv_paper(query:str, debug:bool=False) -> list[ArxivPaper]:
 
     else:
         logger.debug("Retrieve 5 arxiv papers regardless of the date.")
-        search = arxiv.Search(query='cat:cs.AI', sort_by=arxiv.SortCriterion.SubmittedDate)
+        search = arxiv.Search(
+            query="cat:cs.AI", sort_by=arxiv.SortCriterion.SubmittedDate
+        )
         papers = []
         for i in client.results(search):
             papers.append(ArxivPaper(i))
